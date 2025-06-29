fdfc93a1db98857555ea45ad050e32692689fb29
dung953 <dungfilenovuotlink@gmail.com>
2025-06-29 21:52:54 +0700

    Add files via upload

---
+++ speed.lua
@@ -0,0 +1,12 @@
+local library = loadstring(game:HttpGet(("https:// raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3'))) ()
+
+local x = library:CreateWindow ("Hack") 
+
+
+local b = x. library-CreateFolder ("Local-player")
+
+b:DestroyGui()
+
+b:Box("Speed", "number", funtion(val) S(Speed) end)
+
+b: Toggle("Speed", funtion(bool) getgenv() - bool Speed(S) end)
